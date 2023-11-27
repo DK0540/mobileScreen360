d0bf4cfc011f15c1371dae1f149ce181f092f152
Tutorial-git <dk0540@digikull.com>
2023-11-28 04:48:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// import Footer from "./Components/Pages/Footer";
+import "./styles.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import EmployeeProductiveDashboard from "./components/EmployeeProductiveDashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/logo" element={<Login />} />
+          <Route
+            path="/EmployeeProductiveDashboard"
+            element={<EmployeeProductiveDashboard />}
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+//=========================================>>
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import Login from "./components/Login";
+// import EmployeeProductiveDashboard from "./components/EmployeeProductiveDashboard";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route
+//           path="/employeeProductiveDashboard"
+//           component={EmployeeProductiveDashboard}
+//         />
+//         <Route
+//           path="/forgotPassword"
+//           render={() => <div>Forgot Password</div>}
+//         />
+//         <Route path="/" component={Login} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
---
+++ src/components/EmployeeProductiveDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeeProductiveDashboard = () => {
+  return (
+    <div>
+      <h2>Employee Productive Dashboard</h2>
+    </div>
+  );
+};
+
+export default EmployeeProductiveDashboard;
